cfeec1399cdbe949d5295d461c11c6aa31805efa
Vlad Mehakovic <vlad@vlads-air.home>
2019-04-02 09:01:06 +1100

    Added spec to Util, refactored code out of monolith method in order to allow for interaction with the platform URL

--- lib/util.rb
+++ lib/util.rb
@@ -11,28 +11,46 @@ module Util
   # data: only for POST requests
   #
   def self.send_request(method, endpoint, token = nil, data = nil)
-    url = BASE_URL + endpoint
-    headers = {
-      content_type: :json,
-      accept: :json
-    }
-    headers['Authorization'] = token if token
     begin
-      # Sends HTTP request to Routific API server
-      response = nil
-      if method == 'GET'
-        response = RestClient.get(url, headers)
-      elsif method == 'POST'
-        response = RestClient.post(url, data.to_json, headers)
-      end
-
-      return JSON.parse(response)
+      execute_request(method, endpoint, token, data)
     rescue => e
-      puts e
-      errorResponse = JSON.parse e.response.body
-      puts "Received HTTP #{e.message}: #{errorResponse["error"]}"
-      nil
+      error_response(e)
     end
+  end
+
+  private
+
+  def self.execute_request(method, endpoint, token = nil, data = nil)
+    response = RestClient::Request.execute(request_params(method, endpoint, token, data))
+    JSON.parse(response)
+  end
+
+  def self.request_params(method, endpoint, token, data)
+    {}.tap do |params|
+      params[:method]  = method.downcase.to_sym
+      params[:url]     = url(endpoint)
+      params[:payload] = data.to_json unless data == nil
+      params[:headers] = headers(token)
+    end
+  end
+
+  def self.headers(token)
+    {}.tap do |header|
+      header[:content_type]   = :json
+      header[:accept]         = :json
+      header['Authorization'] = token if token
+    end
+  end
+
+  def self.url(endpoint)
+    BASE_URL + endpoint
+  end
+
+  def self.error_response(e)
+    puts e
+    errorResponse = JSON.parse e.response.body
+    puts "Received HTTP #{ e.message }: #{ errorResponse["error"] }"
+    nil
   end
 
   def self.prefix_token(token)
---
+++ spec/util_spec.rb
@@ -0,0 +1,44 @@
+require_relative './helper/spec_helper'
+
+RSpec.describe Util do
+  let(:subject)  { Util }
+  let(:endpoint) { "endpoint" }
+  let(:url)      { "#{ Util::BASE_URL }#{ endpoint }" }
+  let(:headers)  { "HEADERS" }
+  let(:data)     { { key: "RESULT" } }
+
+  before do
+    expect(subject).to receive(:headers)
+      .and_return(headers)
+  end
+
+  describe ".send_request" do
+    context "GET" do
+      let(:method) { "GET" }
+      let(:result) { subject.send_request(method, endpoint ) }
+
+      before do
+        expect(RestClient::Request).to receive(:execute).with(method: :get, url: url, headers: headers)
+          .and_return(data.to_json)
+      end
+
+      it "passes the method to RestClient" do
+        expect(result['key']).to eq("RESULT")
+      end
+    end
+
+    context "POST" do
+      let(:method) { "POST" }
+      let(:result) { subject.send_request(method, endpoint, nil, data ) }
+
+      before do
+        expect(RestClient::Request).to receive(:execute).with(method: :post, url: url, payload: data.to_json, headers: headers)
+          .and_return(data.to_json)
+      end
+
+      it "passes the method to RestClient" do
+        expect(result['key']).to eq("RESULT")
+      end
+    end
+  end
+end
